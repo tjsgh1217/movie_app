d1c38bc1e22db556aeba5a8798673066e64f9a70
김선호 <kimseonho@gimseonhoui-MacBookPro.local>
2020-08-01 16:45:25 +0900

    Updates

---
+++ precache-manifest.7ded2dfa5f8376ab2362612e19ef932f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f1addff4ec3c6facaed5b70189bcc6",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "bccbac4f1375d1d7c3ce",
+    "url": "/movie_app/static/css/main.47afaf7d.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bccbac4f1375d1d7c3ce",
+    "url": "/movie_app/static/js/main.2c251be8.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
