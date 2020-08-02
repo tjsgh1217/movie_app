a0b971fb91c0cfd5190dffe718dbe0c0ebc74d8f
김선호 <kimseonho@gimseonhoui-MacBookPro.local>
2020-08-02 14:28:57 +0900

    React movie_app

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
 
 function Movie({ year, title, summary, poster, genres }) {
     return (
+        <Link to={{
+            pathname:"/movie-detail",
+            state: {
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}>
         <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="genres">{genres.map((genre, index) => (<li key={index} className="genres__genre">{genre}</li>))}
+                <ul className="genres">{genres.map((genre, index) =>
+                    (<li key={index} className="movie__genres">{genre}</li>))}
                 </ul>
-                <p className="movie_summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 120)}...</p>
             </div>
         </div>
+        </Link>
     );
 }
 
---
+++ src/routes/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./About.css";
+
+
+
+function About(props) {
+    return (
+        <div className="about__container">
+      <span>
+        React Moive App
+      </span>
+      <span> 선호의 화려한 작품 </span>
+      </div>
+    )
+}
+
+export default About;
